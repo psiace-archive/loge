85a059216c29497edd294c8ced8ab1ab7cd5f8b5
Chojan Shang <psiace@outlook.com>
2020-01-19 22:08:07 +0800

    :bulb: Documenting source code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+#![deny(warnings)]
+#![deny(missing_docs)]
+
+//! A logger configured via an environment variable which writes cancer to
+//! standard error with colored output for log levels.
+//!
+//! ## Example
+//!
+//! ```
+//! extern crate loge;
+//! #[macro_use] extern crate log;
+//!
+//! use std::env;
+//!
+//! fn main() {
+//!     env::set_var("RUST_LOG", "trace");
+//!     loge::init();
+//!
+//!     trace!("this is trace level");
+//!     debug!("meet a note");
+//!     info!("everything is normal");
+//!     warn!("be careful");
+//!     error!("something error");
+//! }
+//! ```
+
 extern crate chrono;
 pub extern crate env_logger;
 extern crate log;
@@ -5,19 +31,57 @@ extern crate log;
 use chrono::Local;
 use env_logger::Builder;
 
+/// Initializes the global logger with a logger named `loge`.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Panics
+///
+/// This function fails to set the global logger if one has already been set.
 #[inline]
 pub fn init() {
     try_init().unwrap();
 }
 
+/// Initializes the global logger with a logger named `loge`.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Errors
+///
+/// This function fails to set the global logger if one has already been set.
 pub fn try_init() -> Result<(), log::SetLoggerError> {
     try_init_custom_env("RUST_LOG")
 }
 
+/// Initialized the global logger with a logger named `loge`, with a custom variable
+/// name.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Panics
+///
+/// This function fails to set the global logger if one has already been set.
 pub fn init_custom_env(environment_variable_name: &str) {
     try_init_custom_env(environment_variable_name).unwrap();
 }
 
+/// Initialized the global logger with a logger named `loge`, with a custom variable
+/// name.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Errors
+///
+/// This function fails to set the global logger if one has already been set.
 pub fn try_init_custom_env(environment_variable_name: &str) -> Result<(), log::SetLoggerError> {
     let mut builder = formatted_builder();
 
@@ -28,6 +92,19 @@ pub fn try_init_custom_env(environment_variable_name: &str) -> Result<(), log::S
     builder.try_init()
 }
 
+/// Returns a `env_logger::Builder` for further customization.
+///
+/// This method will return a colored and formatted) `env_logger::Builder`
+/// for further customization. Refer to env_logger::Build crate documentation
+/// for further details and usage.
+///
+/// This should be called early in the execution of a Rust program, and the
+/// global logger may only be initialized once. Future initialization attempts
+/// will return an error.
+///
+/// # Errors
+///
+/// This function fails to set the global logger if one has already been set.
 pub fn formatted_builder() -> Builder {
     use std::io::Write;
 
