fc19385caa3fbaf141c67402bd023229e19db0eb
Chojan Shang <psiace@outlook.com>
2020-01-29 14:02:55 +0800

    :white_check_mark: Add jsonified log test.

---
+++ tests/simple-jsonified-log.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+mod tests {
+    extern crate loge;
+    use log::{debug, error, info, trace, warn};
+    use std::env;
+
+    #[test]
+    fn test_fileline_loge() {
+        env::set_var("RUST_LOG", "trace");
+        loge::init_jsonified();
+
+        self::deep();
+        debug!("deboogging");
+        info!("such information");
+        warn!("o_O");
+        error!("boom");
+    }
+
+    pub fn deep() {
+        trace!("one level deep!");
+    }
+}
