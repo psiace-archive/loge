e5de843b0a760a4ec60d4f9eaaf5ba551ad74290
github-action-benchmark <github@users.noreply.github.com>
2020-04-12 17:09:59 +0000

    add Rust Benchmark (cargo) benchmark result for 90c5e5c627c24cdf15ca2c26e3a19b2a7f9b8f28

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1586710613204,
+  "lastUpdate": 1586711399350,
   "repoUrl": "https://github.com/PsiACE/loge",
   "entries": {
     "Rust Benchmark": [
@@ -47,6 +47,54 @@ window.BENCHMARK_DATA = {
             "name": "b40_suppressed_logs",
             "value": 253123,
             "range": "± 74541",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "psiace@outlook.com",
+            "name": "Chojan Shang",
+            "username": "PsiACE"
+          },
+          "committer": {
+            "email": "psiace@outlook.com",
+            "name": "Chojan Shang",
+            "username": "PsiACE"
+          },
+          "distinct": true,
+          "id": "90c5e5c627c24cdf15ca2c26e3a19b2a7f9b8f28",
+          "message": ":green_heart: Keep files.",
+          "timestamp": "2020-04-13T01:08:45+08:00",
+          "tree_id": "ead55207785c985ce2627796b05308e347fb74c2",
+          "url": "https://github.com/PsiACE/loge/commit/90c5e5c627c24cdf15ca2c26e3a19b2a7f9b8f28"
+        },
+        "date": 1586711398332,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "b10_no_logger_active",
+            "value": 51,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "b20_initialize_logger",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "b30_relevant_logs",
+            "value": 231229,
+            "range": "± 26855",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "b40_suppressed_logs",
+            "value": 215849,
+            "range": "± 31661",
             "unit": "ns/iter"
           }
         ]
